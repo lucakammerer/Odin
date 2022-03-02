aba32ec985d15b6183808bcf6f79a03499874a42
lucakammerer <german.memeon@gmail.com>
2022-03-02 14:57:18 +0100

    Added first triangle

--- Odin/src/Platform/OpenGL/OpenGLContext.cpp
+++ Odin/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,10 +17,15 @@ namespace Odin {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		OD_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		OD_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		OD_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		OD_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
 	{
+
 		glfwSwapBuffers(m_WindowHandle);
 	}
 
