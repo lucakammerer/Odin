d259580a36abcd400fa142ef3759277cc4addabe
lucakammerer <german.memeon@gmail.com>
2022-03-03 23:28:13 +0100

    Moved Renderer specific content from Application to SandboxApp

--- Odin/src/Odin.h
+++ Odin/src/Odin.h
@@ -12,6 +12,17 @@
 
 #include "Odin/ImGui/ImGuiLayer.h"
 
+// --- Renderer ---
+#include "Odin/Renderer/Renderer.h"
+#include "Odin/Renderer/RenderCommand.h"
+
+#include "Odin/Renderer/Buffer.h"
+#include "Odin/Renderer/Shader.h"
+#include "Odin/Renderer/VertexArray.h"
+
+#include "Odin/Renderer/OrthographicCamera.h"
+// --- -------- ---
+
 // --- Entry Point ---
 #include "Odin/EntryPoint.h"
-// --- Entry Point ---
+// --- ----------- ---
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -6,34 +6,150 @@ class ExampleLayer : public Odin::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example")
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
 	{
+		m_VertexArray.reset(Odin::VertexArray::Create());
+
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f,
+		};
+
+		std::shared_ptr<Odin::VertexBuffer> vertexBuffer;
+		vertexBuffer.reset(Odin::VertexBuffer::Create(vertices, sizeof(vertices)));
+		Odin::BufferLayout layout = {
+			{ Odin::ShaderDataType::Float3, "a_Position" },
+			{ Odin::ShaderDataType::Float4, "a_Color" },
+		};
+		vertexBuffer->SetLayout(layout);
+		m_VertexArray->AddVertexBuffer(vertexBuffer);
+
+		uint32_t indices[3] = { 0, 1, 2 };
+		std::shared_ptr<Odin::IndexBuffer> indexBuffer;
+		indexBuffer.reset(Odin::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+		m_VertexArray->SetIndexBuffer(indexBuffer);
+
+		m_SquareVA.reset(Odin::VertexArray::Create());
+
+		float squareVertices[3 * 4] = {
+		   -0.75f, -0.75f, 0.0f,
+			0.75f, -0.75f, 0.0f,
+			0.75f,  0.75f, 0.0f,
+		   -0.75f,  0.75f, 0.0f
+		};
+
+		std::shared_ptr<Odin::VertexBuffer> squareVB;
+		squareVB.reset(Odin::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+		squareVB->SetLayout({
+			{ Odin::ShaderDataType::Float3, "a_Position" }
+			});
+		m_SquareVA->AddVertexBuffer(squareVB);
+
+		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
+		std::shared_ptr<Odin::IndexBuffer> squareIB;
+		squareIB.reset(Odin::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
+		m_SquareVA->SetIndexBuffer(squareIB);
+
+		std::string vertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
+			uniform mat4 u_ViewProjection;
+
+			out vec3 v_Position;
+			out vec4 v_Color;
+
+			void main()
+			{
+				v_Position = a_Position;
+				v_Color = a_Color;
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+			in vec4 v_Color;
+
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
+			}
+		)";
+
+		m_Shader.reset(new Odin::Shader(vertexSrc, fragmentSrc));
+
+		std::string blueShaderVertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+
+			uniform mat4 u_ViewProjection;
+
+			out vec3 v_Position;
+
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string blueShaderFragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+
+			void main()
+			{
+				color = vec4(0.2, 0.3, 0.8, 1.0);
+			}
+		)";
+
+		m_BlueShader.reset(new Odin::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
 	}
 
 	void OnUpdate() override
 	{
-		if (Odin::Input::IsKeyPressed(OD_KEY_TAB))
-			OD_TRACE("Tab key is pressed (poll)!");
+		Odin::RenderCommand::SetClearColor({ 0.1f, 0.05f, 0.1f, 1 });
+		Odin::RenderCommand::Clear();
+
+		m_Camera.SetRotation(45.0f);
+		m_Camera.SetPosition({ 0.5f, 0.5f, 0.0f });
+
+		Odin::Renderer::BeginScene(m_Camera);
+
+		Odin::Renderer::Submit(m_BlueShader, m_SquareVA);
+		Odin::Renderer::Submit(m_Shader, m_VertexArray);
+
+		Odin::Renderer::EndScene();
 	}
 
 	virtual void OnImGuiRender() override
 	{
-		ImGui::Begin("Test");
-		ImGui::Text("Hello World");
-		ImGui::End();
 	}
 
 	void OnEvent(Odin::Event& event) override
 	{
-		if (event.GetEventType() == Odin::EventType::KeyPressed)
-		{
-			Odin::KeyPressedEvent& e = (Odin::KeyPressedEvent&)event;
-			if (e.GetKeyCode() == OD_KEY_TAB)
-				OD_TRACE("Tab key is pressed (event)!");
-			OD_TRACE("{0}", (char)e.GetKeyCode());
-		}
 	}
+private:
+	std::shared_ptr<Odin::Shader> m_Shader;
+	std::shared_ptr<Odin::VertexArray> m_VertexArray;
 
+	std::shared_ptr<Odin::Shader> m_BlueShader;
+	std::shared_ptr<Odin::VertexArray> m_SquareVA;
+
+	Odin::OrthographicCamera m_Camera;
 };
 
 class Sandbox : public Odin::Application
