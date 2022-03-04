29c8134355b9bb5a9a52418a36da137a0cc91f37
lucakammerer <german.memeon@gmail.com>
2022-03-04 01:00:44 +0100

    Transformation

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,11 +2,13 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Odin::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_SquarePosition(0.0f)
 	{
 		m_VertexArray.reset(Odin::VertexArray::Create());
 
@@ -31,12 +33,12 @@ public:
 		m_VertexArray->SetIndexBuffer(indexBuffer);
 
 		m_SquareVA.reset(Odin::VertexArray::Create());
-
+		
 		float squareVertices[3 * 4] = {
-		   -0.75f, -0.75f, 0.0f,
-			0.75f, -0.75f, 0.0f,
-			0.75f,  0.75f, 0.0f,
-		   -0.75f,  0.75f, 0.0f
+		   -0.5f, -0.5f, 0.0f,
+		    0.5f, -0.5f, 0.0f,
+			0.5f,  0.5f, 0.0f,
+		   -0.5f,  0.5f, 0.0f,
 		};
 
 		std::shared_ptr<Odin::VertexBuffer> squareVB;
@@ -58,6 +60,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -66,7 +69,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -93,13 +96,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -123,13 +127,11 @@ public:
 	{
 		if (Odin::Input::IsKeyPressed(OD_KEY_LEFT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
-
 		else if (Odin::Input::IsKeyPressed(OD_KEY_RIGHT))
 			m_CameraPosition.x += m_CameraMoveSpeed * ts;
 
 		if (Odin::Input::IsKeyPressed(OD_KEY_UP))
 			m_CameraPosition.y += m_CameraMoveSpeed * ts;
-
 		else if (Odin::Input::IsKeyPressed(OD_KEY_DOWN))
 			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
 
@@ -146,7 +148,18 @@ public:
 
 		Odin::Renderer::BeginScene(m_Camera);
 
-		Odin::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Odin::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+		
 		Odin::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Odin::Renderer::EndScene();
